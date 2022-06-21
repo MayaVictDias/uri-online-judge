2fe994528ee1f7b9813f5f25149e570dee6a67d5
mayavictdias <victdias.mayara@icloud.com>
2022-06-20 21:00:36 -0300

    Resolucao do exercicio 1009

---
+++ src/iniciante/uri1009.java
@@ -0,0 +1,29 @@
+package iniciante;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class uri1009 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		// Ler nome do vendedor
+		String nomeVendedor = sc.nextLine();
+		// Salario fixo do vendedor
+		double salario = sc.nextDouble();
+		// Total de vendas efetuadas
+		double totalVendasEfetuadas = sc.nextDouble();
+		
+		// O vendedor recebe 15% de comissão nas vendas efetuadas
+		double comissao = totalVendasEfetuadas * 0.15;
+		double valorAReceber = salario + comissao;
+		
+		System.out.printf("TOTAL = %.2f%n", valorAReceber);
+
+	}
+
+}
