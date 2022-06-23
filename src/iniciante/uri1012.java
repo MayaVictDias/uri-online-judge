efca15e6eb34c50b7fec68d21f07711282db06de
mayavictdias <victdias.mayara@icloud.com>
2022-06-22 21:56:54 -0300

    Resolucao do exercicio 1013

--- src/iniciante/uri1012.java
+++ src/iniciante/uri1012.java
@@ -46,7 +46,7 @@ public class uri1012 {
 		System.out.printf("CIRCULO: %.3f%n", areaCirculo);
 		System.out.printf("TRAPEZIO: %.3f%n", areaTrapezio);
 		System.out.printf("QUADRADO: %.3f%n", areaQuadrado);
-		System.out.printf("TRIANGULO: %.3f%n", areaRetangulo);
+		System.out.printf("RETANGULO: %.3f%n", areaRetangulo);
 
 	}
 
---
+++ src/iniciante/uri1013.java
@@ -0,0 +1,44 @@
+package iniciante;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * @author mayavictdias
+ *
+ */
+public class uri1013 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b, c, maiorAB, maiorNumero;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		
+		sc.close();
+		
+		// Formula para calcular se qual é maior: a ou b
+		maiorAB = (a + b + Math.abs(a - b)) / 2;
+		maiorNumero = (maiorAB + c + Math.abs(maiorAB - c)) / 2;
+		
+		/*
+		if (a > b && a > c) {
+			maiorAB = a;
+		} else if (b > a && b > c) {
+			maiorAB = b;
+		} else {
+			maiorAB = c;
+		}
+		*/
+		
+		System.out.println(maiorNumero + " eh o maior");
+
+	}
+
+}
