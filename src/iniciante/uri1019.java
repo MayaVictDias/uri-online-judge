feccb8f62a1117fa2ec617a7ff7c3aedd4113b0b
mayavictdias <victdias.mayara@icloud.com>
2022-06-24 16:17:58 -0300

    Resolucao do exercicio 1019

---
+++ src/iniciante/uri1019.java
@@ -0,0 +1,30 @@
+package iniciante;
+
+import java.util.Scanner;
+
+public class uri1019 {
+
+	public static void main(String[] args) {
+		/*
+		 * Leia um valor inteiro, que é o tempo de duração em 
+		 * segundos de um determinado evento em uma fábrica, 
+		 * e informe-o expresso no formato horas:minutos:segundos.
+		 */
+		
+		int segundos, horas, minutos; 
+		
+		Scanner sc = new Scanner(System.in);
+		
+		segundos = sc.nextInt();
+		
+		sc.close();
+		
+		horas = segundos / 3600;
+		minutos = (segundos % 3600) / 60;
+		segundos = segundos % 60;
+		
+		System.out.println(horas + ":" + minutos + ":" + segundos);
+
+	}
+
+}
